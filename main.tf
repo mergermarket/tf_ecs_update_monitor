91ca9517aea02b573cd570a2b458e3d5d664c11e
Tom Yandell <tom@tomyandell.co>
2017-06-22 08:07:00 +0000

    Terraform resources
    Applies the provisioner to an ECS service.
    
    JIRA: PLAT-1003

---
+++ main.tf
@@ -0,0 +1,32 @@
+variable "cluster" {
+  description = "The ECS cluster that the service is deployed to."
+  type = "string"
+}
+
+variable "service" {
+  description = "The name of the ECS service."
+  type = "string"
+}
+
+variable "taskdef" {
+  description = "The task definition ARN that the service is being updated to."
+  type = "string"
+}
+
+variable "region" {
+  description = "The AWS region where the service is deployed."
+  type = "string"
+}
+
+resource "null_resource" "ecs_update_monitor" {
+  triggers {
+    cluster = "${var.cluster}"
+    service = "${var.service}"
+    taskdef = "${var.taskdef}"
+    region = "${var.region}"
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/provision.sh '${path.module}' '${var.cluster}' '${var.service}' '${var.taskdef}' '${var.region}'"
+  }
+}
---
+++ provision.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+if [ "$#" != "5" ]; then
+    echo 'Usage: provision.sh <module-root> <cluster> <service> <taskdef> <region>' >&2
+    exit 1
+fi
+
+python -m ecs_update_monitor --cluster "$2" \
+                             --service "$3" \
+                             --taskdef "$4" \
+                             --region  "$5"
