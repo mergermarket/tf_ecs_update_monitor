a141fb4b9da0a840be3cd8bfa85c8bfacffeea0b
Tom Yandell <tom@tomyandell.co>
2017-07-04 09:33:34 +0000

    Terraform format.

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 variable "cluster" {
   description = "The ECS cluster that the service is deployed to."
-  type = "string"
+  type        = "string"
 }
 
 variable "service" {
   description = "The name of the ECS service."
-  type = "string"
+  type        = "string"
 }
 
 variable "taskdef" {
   description = "The task definition ARN that the service is being updated to."
-  type = "string"
+  type        = "string"
 }
 
 variable "region" {
   description = "The AWS region where the service is deployed."
-  type = "string"
+  type        = "string"
 }
 
 resource "null_resource" "ecs_update_monitor" {
@@ -23,7 +23,7 @@ resource "null_resource" "ecs_update_monitor" {
     cluster = "${var.cluster}"
     service = "${var.service}"
     taskdef = "${var.taskdef}"
-    region = "${var.region}"
+    region  = "${var.region}"
   }
 
   provisioner "local-exec" {
