cb15ae16f4d6e83ef3c2ee8e7140f720dbe0c4bf
Keir Badger <keir.badger@acuris.com>
2022-06-30 16:24:42 +0100

    Update __init__.py

--- ecs_update_monitor/__init__.py
+++ ecs_update_monitor/__init__.py
@@ -111,6 +111,7 @@ class ECSEventIterator:
         )
 
     def _check_taskdef(self, primary_deployment):
+        sleep(15)
         if primary_deployment['taskDefinition'] != self._taskdef:
             raise TaskdefDoesNotMatchError(
                 self._taskdef, primary_deployment['taskDefinition']
